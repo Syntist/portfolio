172098c09cb10dbc073e07f3bfb89d3faaf0d422
Syntist <syed.talha.khalid@gmail>
2024-03-25 20:05:06 -0400

    removed unwanted console

--- src/server-action/github.js
+++ src/server-action/github.js
@@ -5,8 +5,6 @@ import baseApi from "@/baseApi";
 export const getRepoData = async (url) => {
   const link = url.split("://")[1].split('/')
 
-	console.log(link)
-
   try {
     const content = await baseApi.get(
       `https://api.github.com/repos/${link[1]}/${link[2]}`
