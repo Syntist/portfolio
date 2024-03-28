92a412c6cc82af66f10dea9a2edd91c6e6820394
syntist <syed.talha.khalid@gmail>
2024-03-29 03:00:24 +0500

    section additions and style

--- src/components/Proficiency/index.js
+++ src/components/Proficiency/index.js
@@ -1,9 +1,183 @@
-import { Typography } from "@mui/material";
+import { Typography, Box, Container } from "@mui/material";
 
 export const Proficiency = (props) => {
   return (
-    <div>
-      <Typography variant="h1">Proficiency</Typography>
-    </div>
+    <Box
+      sx={{
+        width: "100%",
+        overflow: "hidden",
+        paddingTop: "50px",
+        paddingBottom: "50px"
+      }}
+    >
+      <Container maxWidth="xl">
+        <Box
+          sx={{
+            marginBottom: "45px"
+          }}
+        >
+          <Typography
+            variant="h2"
+            sx={{
+              fontSize: "40px",
+              lineHeight: "1.5",
+              display: "flex",
+              width: "100%",
+              alignItems: "flex-end",
+              gap: "24px",
+              fontWeight: "400",
+
+              "&:after": {
+                content: "''",
+                flexGrow: "1",
+                display: "block",
+                height: "1px",
+                backgroundColor: "currentcolor",
+                opacity: "0.1",
+                position: "relative",
+                top: "-9px"
+              }
+            }}
+          >
+            Proficiency
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+
+            "> div + div": {
+              marginTop: "24px"
+            }
+          }}
+        >
+          <Box>
+            <Typography
+              variant="h3"
+              sx={{
+                fontSize: "26px",
+                lineHeight: "1.5",
+                color: "#e9e9e9",
+                fontWeight: "400",
+                marginBottom: "11px"
+              }}
+            >
+              Frontend / Design
+            </Typography>
+            <Box
+              sx={{
+
+                "progress": {
+                  width: "100%",
+                  display: "block",
+                  backgroundColor: "#373737",
+                  borderRadius: "4px",
+
+                  "&::-webkit-progress-bar": {
+                    backgroundColor: "#373737",
+                    borderRadius: "4px"
+                  },
+
+                  "&::-webkit-progress-value": {
+                    backgroundColor: "#E3C050",
+                    borderRadius: "4px"
+                  },
+
+                  "&[value]::-moz-progress-bar": {
+                    backgroundColor: "#E3C050",
+                    borderRadius: "4px"
+                  }
+                }
+              }}
+            >
+              <progress value={'65'} max={'100'}></progress>
+            </Box>
+          </Box>
+          <Box>
+            <Typography
+              variant="h3"
+              sx={{
+                fontSize: "26px",
+                lineHeight: "1.5",
+                color: "#e9e9e9",
+                fontWeight: "400",
+                marginBottom: "11px"
+              }}
+            >
+              Backend
+            </Typography>
+            <Box
+              sx={{
+
+                "progress": {
+                  width: "100%",
+                  display: "block",
+                  backgroundColor: "#373737",
+                  borderRadius: "4px",
+
+                  "&::-webkit-progress-bar": {
+                    backgroundColor: "#373737",
+                    borderRadius: "4px"
+                  },
+
+                  "&::-webkit-progress-value": {
+                    backgroundColor: "#E3C050",
+                    borderRadius: "4px"
+                  },
+
+                  "&[value]::-moz-progress-bar": {
+                    backgroundColor: "#E3C050",
+                    borderRadius: "4px"
+                  }
+                }
+              }}
+            >
+              <progress value={'85'} max={'100'}></progress>
+            </Box>
+          </Box>
+          <Box>
+            <Typography
+              variant="h3"
+              sx={{
+                fontSize: "26px",
+                lineHeight: "1.5",
+                color: "#e9e9e9",
+                fontWeight: "400",
+                marginBottom: "11px"
+              }}
+            >
+              Programming
+            </Typography>
+            <Box
+              sx={{
+
+                "progress": {
+                  width: "100%",
+                  display: "block",
+                  backgroundColor: "#373737",
+                  borderRadius: "4px",
+
+                  "&::-webkit-progress-bar": {
+                    backgroundColor: "#373737",
+                    borderRadius: "4px"
+                  },
+
+                  "&::-webkit-progress-value": {
+                    backgroundColor: "#E3C050",
+                    borderRadius: "4px"
+                  },
+
+                  "&[value]::-moz-progress-bar": {
+                    backgroundColor: "#E3C050",
+                    borderRadius: "4px"
+                  }
+                }
+              }}
+            >
+              <progress value={'75'} max={'100'}></progress>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
   );
 };
--- src/components/WhatIDoSection/index.js
+++ src/components/WhatIDoSection/index.js
@@ -1,17 +1,241 @@
-import { Typography } from "@mui/material";
+import { Typography, Box, Container, Grid, List, ListItem } from "@mui/material";
+import Image from 'next/image'
+import HtmlIcon from '@mui/icons-material/Html';
 
 export const WhatIDoSection = (props) => {
   return (
-    <div>
-      <Typography variant="h1">What I do</Typography>
-      <p>
-        CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK html-5
-        css3 sass JavaScript reactjs nodejs swift npm sql-database aws firebase
-        python docker ⚡ Develop highly interactive Front end / User Interfaces
-        for your web and mobile applications ⚡ Progressive Web Applications (
-        PWA ) in normal and SPA Stacks ⚡ Integration of third party services
-        such as Firebase/ AWS / Digital Ocean
-      </p>
-    </div>
+    <Box
+      sx={{
+        width: "100%",
+        overflow: "hidden",
+        paddingTop: "50px",
+        paddingBottom: "50px"
+      }}
+    >
+      <Container maxWidth="xl">
+        <Grid container spacing={4}>
+          <Grid
+            item
+            xs={6}
+          >
+            <Box
+              sx={{
+                overflow: "hidden",
+                borderRadius: "10px",
+
+                "img": {
+                  width: "100%",
+                  height: "auto",
+                  maxWidth: "100%"
+                }
+              }}
+            >
+              <Image
+                src={'/img02.gif'}
+                width={500}
+                height={500}
+                alt=""
+              />
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={6}
+            sx={{
+              display: "flex"
+            }}
+          >
+            <Box
+              sx={{
+                marginTop: "auto",
+                marginBottom: "auto"
+              }}
+            >
+              <Box
+                sx={{
+                  marginBottom: "45px"
+                }}
+              >
+                <Typography
+                  variant="h2"
+                  sx={{
+                    fontSize: "40px",
+                    lineHeight: "1.5",
+                    display: "flex",
+                    width: "100%",
+                    alignItems: "flex-end",
+                    gap: "24px",
+                    fontWeight: "400",
+
+                    "&:after": {
+                      content: "''",
+                      flexGrow: "1",
+                      display: "block",
+                      height: "1px",
+                      backgroundColor: "currentcolor",
+                      opacity: "0.1",
+                      position: "relative",
+                      top: "-9px"
+                    }
+                  }}
+                >
+                  What I do
+                </Typography>
+                <Typography
+                  variant="h3"
+                  sx={{
+                    fontSize: "25px",
+                    lineHeight: "1.2",
+                    textTransform: "uppercase",
+                    color: "#c0c0c0"
+                  }}
+                >
+                  CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexWrap: "wrap",
+                  gap: "20px",
+                  marginBottom: "24px"
+                }}
+              >
+                <Box
+                  sx={{
+                    padding: "10px",
+                    border: "1px solid #373737",
+                    backgroundColor: "#2b2b2b",
+                    borderRadius: "4px",
+
+                    "img": {
+                      maxWidth: "100%",
+                      height: "auto"
+                    }
+                  }}
+                >
+                  <Image
+                    src={'/html5-badge-h-solo.png'}
+                    width={64}
+                    height={64}
+                    alt=""
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    padding: "10px",
+                    border: "1px solid #373737",
+                    backgroundColor: "#2b2b2b",
+                    borderRadius: "4px",
+
+                    "img": {
+                      maxWidth: "100%",
+                      height: "auto"
+                    }
+                  }}
+                >
+                  <Image
+                    src={'/icons8-css-logo-128.png'}
+                    width={64}
+                    height={64}
+                    alt=""
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    padding: "10px",
+                    border: "1px solid #373737",
+                    backgroundColor: "#2b2b2b",
+                    borderRadius: "4px",
+
+                    "img": {
+                      maxWidth: "100%",
+                      height: "auto"
+                    }
+                  }}
+                >
+                  <Image
+                    src={'/icons8-react-100.png'}
+                    width={64}
+                    height={64}
+                    alt=""
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    padding: "10px",
+                    border: "1px solid #373737",
+                    backgroundColor: "#2b2b2b",
+                    borderRadius: "4px",
+
+                    "img": {
+                      maxWidth: "100%",
+                      height: "auto"
+                    }
+                  }}
+                >
+                  <Image
+                    src={'/icons8-node-js-128.png'}
+                    width={64}
+                    height={64}
+                    alt=""
+                  />
+                </Box>
+                <Box
+                  sx={{
+                    padding: "10px",
+                    border: "1px solid #373737",
+                    backgroundColor: "#2b2b2b",
+                    borderRadius: "4px",
+
+                    "img": {
+                      maxWidth: "100%",
+                      height: "auto"
+                    }
+                  }}
+                >
+                  <Image
+                    src={'/icons8-npm-logo.svg'}
+                    width={64}
+                    height={64}
+                    alt=""
+                  />
+                </Box>
+              </Box>
+              <List
+                sx={{
+
+                  "> li + li": {
+                    marginTop: "10px"
+                  }
+                }}
+              >
+                <ListItem
+                  sx={{
+                    borderLeft: "8px solid #2b2b2b"
+                  }}
+                >
+                  ⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications
+                </ListItem>
+                <ListItem
+                  sx={{
+                    borderLeft: "8px solid #2b2b2b"
+                  }}
+                >
+                  ⚡ Progressive Web Applications (PWA) in normal and SPA Stacks
+                </ListItem>
+                <ListItem
+                  sx={{
+                    borderLeft: "8px solid #2b2b2b"
+                  }}
+                >
+                  ⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean
+                </ListItem>
+              </List>
+            </Box>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 };
