d5757cbbbe59cf42a26643e695ae929fe8c63423
Syed Talha Khalid <syed.talha.khalid@gmail.com>
2025-08-20 13:03:24 -0400

    removed blog

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        // Match any path on the blog subdomain
-        source: '/:slug*',
-        has: [
-          {
-            type: 'host',
-            value: 'blog.myurl.com',
-          },
-        ],
-        // Serve content from pages/blog/[slug].js
-        destination: '/blog/:slug*',
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
