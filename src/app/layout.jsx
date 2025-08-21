3d59f5e74067854aed1e46d1700bd4ddfa95c0d6
Syed Talha Khalid <syed.talha.khalid@gmail.com>
2025-08-20 20:21:18 -0400

    website meta data update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -21,6 +21,25 @@ const oswald = Oswald({
 export const metadata = {
   title: "Syed Talha Khalid",
   description: "Crafting Scalable & Delightful Digital Experiences",
+  icons: {
+    icon: "/favicon.ico", // place this in /public
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png", // optional, for iOS
+  },
+  openGraph: {
+    title: "Syed Talha Khalid",
+    description: "Crafting Scalable & Delightful Digital Experiences",
+    url: "https://www.syedtalhakhalid.com", // <-- your deployed domain
+    siteName: "Syed Talha Khalid",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Syed Talha Khalid",
+    description: "Crafting Scalable & Delightful Digital Experiences",
+    images: ["https://www.syedtalhakhalid.com/preview.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
